4e1552088307c7349a986183f50aba52a2ab8a51
Renat Eskenin <3draven@gmail.com>
2024-10-13 20:31:47 +0200

    Lua fix for autocomplete

--- resources/scripts/java/visitors/components/schemas/schema_end.lua
+++ resources/scripts/java/visitors/components/schemas/schema_end.lua
@@ -1,7 +1,7 @@
 --- This visitor is invoked after processing any kind of schema
 --- @param namesStack ModelName[] # chain of model names from root to this point
 --- @param extensions table # table with free form with "x-" OpenAPI extensions for this level of spec
---- @param schemaDescriptor SchemaDescriptor # table with free form with "x-" OpenAPI extensions for this level of spec
+--- @param schemaDescriptor Schema # table with free form with "x-" OpenAPI extensions for this level of spec
 --- @param callsStack Script[] # An array of Script objects representing the sequence of scripts executed in the visitor call chain
 --- @return WriteOperation[] # Returns the output code and  file name for writing code
 function visitSchemaEnd(namesStack, schemaDescriptor, extensions, callsStack)
--- resources/scripts/java/visitors/tags/spec_tags_end.lua
+++ resources/scripts/java/visitors/tags/spec_tags_end.lua
@@ -1,5 +1,5 @@
 --- This visitor is invoked at the start of OpenAPI scpec after processing tags on by one
---- @param tags TagDescriptor[] # OpenAPI described servers
+--- @param tags Tag[] # OpenAPI described servers
 --- @param extensions table<string,any> # table with free form with "x-" OpenAPI extensions for this level of spec (root level)
 --- @param callsStack Script[] # An array of Script objects representing the sequence of scripts executed in the visitor call chain
 --- @return WriteOperation[] # Returns the output code and  file name for writing code
